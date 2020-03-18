b75dedd3232d7115fa4837084954374f604c5860
kristina <kristinaa.stojanovska@gmail.com>
2020-03-18 14:32:37 -0700

    Updates

---
+++ precache-manifest.6bc8a89dada174eafdc74996fde3faf5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f587efa1bab72aba4689061c80c640",
+    "url": "/restaurants-finder/index.html"
+  },
+  {
+    "revision": "84bf6f45865618c9c49d",
+    "url": "/restaurants-finder/static/css/main.c5565319.chunk.css"
+  },
+  {
+    "revision": "dc9f9c9b0dc4dc21cb93",
+    "url": "/restaurants-finder/static/js/2.125ee2d8.chunk.js"
+  },
+  {
+    "revision": "84bf6f45865618c9c49d",
+    "url": "/restaurants-finder/static/js/main.4f4307b2.chunk.js"
+  },
+  {
+    "revision": "eb6f3eac3ca6bdc7f6e6",
+    "url": "/restaurants-finder/static/js/runtime~main.0779a698.js"
+  },
+  {
+    "revision": "2f7c3a7d5282c1eef23b703d731707ae",
+    "url": "/restaurants-finder/static/media/pasta.2f7c3a7d.jpg"
+  },
+  {
+    "revision": "007361755058d2fb7cdbbf8daf631275",
+    "url": "/restaurants-finder/static/media/pizza.00736175.jpg"
+  },
+  {
+    "revision": "8b441d161da3468af9ee805376cb0014",
+    "url": "/restaurants-finder/static/media/smaller-half.8b441d16.jpg"
+  },
+  {
+    "revision": "6187d757762778362114a4e62ac6e696",
+    "url": "/restaurants-finder/static/media/spaghetti.6187d757.jpg"
+  }
+]);
